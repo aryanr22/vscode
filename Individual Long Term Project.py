9083e2511d0d0e6675a477750112f268769d0aff
Aryan <aryan.rahman234@gmail.com>
2023-10-04 22:33:00 -0400

    modified:   Individual Long Term Project.py

--- Individual Long Term Project.py
+++ Individual Long Term Project.py
@@ -6,7 +6,7 @@ DecimalRaptorOffense = []
 ShortenedRaptorDefense = []
 DecimalRaptorDefense = []
 
-ShortenedRaptorTotalTotal = []
+ShortenedRaptorTotal = []
 DecimalRaptorTotal = []
 
 with open('RAPTOR 2022-23.csv') as file:
@@ -21,3 +21,26 @@ with open('RAPTOR 2022-23.csv') as file:
 
 		s_t = float(row['raptor_offense']) // 1
 		d_t = float(row['raptor_offense']) % 1
+
+		ShortenedRaptorOffense.append(s_o)
+		DecimalRaptorOffense.append(d_o)
+
+		ShortenedRaptorDefense.append(s_d)
+		DecimalRaptorDefense.append(d_d)
+
+		ShortenedRaptorTotal.append(s_t)
+		DecimalRaptorTotal.append(d_t)
+
+NewValues = []
+for x in range(len(ShortenedRaptorOffense)):
+    row = [ShortenedRaptorOffense[x], DecimalRaptorOffense[x], ShortenedRaptorDefense[x], DecimalRaptorDefense[x], ShortenedRaptorTotal[x], DecimalRaptorTotal[x]]
+NewValues.append(row)
+
+with open(r'RAPTOR 2022-23 With New Data.csv', 'w', newline="\n") as outfile:
+    wr = csv.writer(outfile)
+    wr.writerow(['offense_shortened', 'offense_decimal', 'defense_shortened', 'defense_decimal', 'total_shortened', 'total_decimal',])
+    wr.writerows(NewValues)
+
+
+
+
