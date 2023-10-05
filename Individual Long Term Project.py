df29ee8321143d86988ba8f67d379736141af23d
Aryan <aryan.rahman234@gmail.com>
2023-10-04 22:01:06 -0400

    deleted:    Condensed RAPTOR 2022-23.csv 	modified:   Individual Long Term Project.py 	deleted:    Long Term Individual Project.py 	deleted:    RAPTOR 2022-23 New Data.xlsx

--- Individual Long Term Project.py
+++ Individual Long Term Project.py
@@ -1,10 +1,23 @@
 import csv
 
 ShortenedRaptorOffense = []
-ShortenedRaptorOffense = []
+DecimalRaptorOffense = []
 
-ShortenedRaptorOffense = []
-ShortenedRaptorOffense = []
+ShortenedRaptorDefense = []
+DecimalRaptorDefense = []
 
-ShortenedRaptorOffense = []
-ShortenedRaptorOffense = []
+ShortenedRaptorTotalTotal = []
+DecimalRaptorTotal = []
+
+with open('RAPTOR 2022-23.csv') as file:
+	rows = csv.DictReader(file)
+
+	for row in rows:
+		s_o = float(row['raptor_offense']) // 1
+		d_o = float(row['raptor_offense']) % 1
+
+		s_d = float(row['raptor_offense']) // 1
+		d_d = float(row['raptor_offense']) % 1
+
+		s_t = float(row['raptor_offense']) // 1
+		d_t = float(row['raptor_offense']) % 1
